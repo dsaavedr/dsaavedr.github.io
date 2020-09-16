ea67f6d24d48f624f6deb3104e9b3924e60ead9f
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-09-16 13:47:02 -0500

    Made navbar change background opacity upon scrolling

--- site/src/components/Navbar.js
+++ site/src/components/Navbar.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../images/logo.svg';
+
+import { FaAlignRight } from 'react-icons/fa';
 
 export default class Navbar extends Component {
     state = {
-        isOpen: false
+        isOpen: false,
+        top: true
+    }
+
+    handleToggle = () => {
+        this.setState({ isOpen: !this.state.isOpen })
+    }
+
+    componentDidMount() {
+        let listener = document.addEventListener('scroll', e => {
+            const scrolled = document.scrollingElement.scrollTop;
+            if (scrolled >= 120) {
+                if (this.state.top) {
+                    this.setState({ top: false })
+                }
+            } else {
+                if (!this.state.top) {
+                    this.setState({ top: true })
+                }
+            }
+        })
     }
 
     render() {
         return (
-            <div>
-                hello from navbar
-            </div>
+            <nav className={this.state.top ? "navbar top" : "navbar"}>
+                <div className="nav-center">
+                    <div className="nav-header">
+                        <Link to="/">
+                            <img src={logo} alt="Logo" />
+                        </Link>
+                        <button className="nav-btn" type="button" onClick={this.handleToggle}>
+                            <FaAlignRight className="nav-icon" />
+                        </button>
+                    </div>
+                    <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+                        <li><Link to="/">Home</Link> </li>
+                        <li><Link to="/">Link 2</Link> </li>
+                        <li><Link to="/">Link 3</Link> </li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
