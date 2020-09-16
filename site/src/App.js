8721cf9ee4dc0f6897bbc01d061cb71f8254cd26
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-09-16 11:37:21 -0500

    Removed utilities submodule, began work on navbar and set router up

--- site/src/App.js
+++ site/src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 import './App.sass';
 
+import Navbar from './components/Navbar';
+
+import Home from './pages/Home';
+import Error from './pages/Error';
+
+import { Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
---
+++ site/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    render() {
+        return (
+            <div>
+                hello from navbar
+            </div>
+        )
+    }
+}
