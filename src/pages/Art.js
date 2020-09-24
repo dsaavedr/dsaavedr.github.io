ceae5d1d3ed1384d180c6d40235383c67f958cdd
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-09-23 22:27:05 -0500

    Added noreferrer rel to a tags to get rid of warnings

--- src/pages/Art.js
+++ src/pages/Art.js
@@ -21,10 +21,10 @@ export default class Art extends Component {
                             A big part of development is user experience. Although its design can be very technical, it involves a great deal of creativity and, along with the design of the page, can turn the end product into a piece of art. Here are a few websites that showcase my point:
                         </p>
                         <ul>
-                            <li><a href="http://www.feedmusic.com/" target="_blank">feedmusic.com</a></li>
-                            <li><a href="https://www.etq-amsterdam.com/" target="_blank">etq-amsterdam.com</a></li>
-                            <li><a href="http://www.mikiyakobayashi.com/" target="_blank">mikiyakobayashi.com</a></li>
-                            <li><a href="http://movie-mark.com/" target="_blank">movie-mark.com</a></li>
+                            <li><a rel="noopener noreferrer" href="http://www.feedmusic.com/" target="_blank">feedmusic.com</a></li>
+                            <li><a rel="noopener noreferrer" href="https://www.etq-amsterdam.com/" target="_blank">etq-amsterdam.com</a></li>
+                            <li><a rel="noopener noreferrer" href="http://www.mikiyakobayashi.com/" target="_blank">mikiyakobayashi.com</a></li>
+                            <li><a rel="noopener noreferrer" href="http://movie-mark.com/" target="_blank">movie-mark.com</a></li>
                         </ul>
                         <p>The immense versatility of these tools not only allows you to create beautiful and functional websites, but stand-alone art, animations and effects. These are some of the things I've come up with:</p>
                     </MainSection>
