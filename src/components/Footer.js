70fd33ac4ec471d6e77fa8767d606ca2b1fb5675
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-11-12 11:49:59 -0500

    Minor change to cr notice and formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import { FaCodepen, FaGithub, FaInstagram, FaEnvelope, FaLinkedin } from 'react-icons/fa';
-import { RiHeart3Line } from 'react-icons/ri';
+import {
+    FaCodepen,
+    FaGithub,
+    FaInstagram,
+    FaEnvelope,
+    FaLinkedin,
+} from "react-icons/fa";
+import { RiHeart3Line } from "react-icons/ri";
 
 export default function Footer() {
     return (
         <footer>
-            <div className="copyright">
-                &copy; Daniel Saavedra. All Rights Reserved.
+            <div className='copyright'>&copy; 2020. All Rights Reserved.</div>
+            <div className='designedBy'>
+                Designed with <RiHeart3Line className='heart' /> by{" "}
+                <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://github.com/dsaavedr'
+                >
+                    Daniel Saavedra
+                </a>
+                .
             </div>
-            <div className="designedBy">
-                Designed with <RiHeart3Line className="heart" /> by <a target="_blank" rel="noopener noreferrer" href="https://github.com/dsaavedr">Daniel Saavedra</a>.
-            </div>
-            <div className="icons">
+            <div className='icons'>
+                {/* prettier-ignore */}
                 <a target="_blank" rel="noopener noreferrer" href="https://codepen.io/dsaavedr"><FaCodepen /></a>
+                {/* prettier-ignore */}
                 <a target="_blank" rel="noopener noreferrer" href="https://github.com/dsaavedr"><FaGithub /></a>
+                {/* prettier-ignore */}
                 <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/danielsm14/"><FaInstagram /></a>
+                {/* prettier-ignore */}
                 <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/daniel-saavedra-988717121/"><FaLinkedin /></a>
+                {/* prettier-ignore */}
                 <a target="_blank" rel="noopener noreferrer" href="mailto:danielsaavedram@hotmail.com"><FaEnvelope /></a>
             </div>
         </footer>
-    )
+    );
 }
