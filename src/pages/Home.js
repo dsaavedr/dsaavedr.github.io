4cde60af0acb507d4e5ed8a6b60cfaf627449bd7
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-11-12 11:49:11 -0500

    Minor change to Hero subtitle (and formatting)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import Hero from '../components/Hero';
-import Banner from '../components/Banner';
-import MainSection from '../components/MainSection';
-import Testimonials from '../components/Testimonials';
-import Carousel from '../components/Carousel';
-import Creators from '../components/Creators';
-import Loading from '../components/Loading';
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import MainSection from "../components/MainSection";
+import Testimonials from "../components/Testimonials";
+import Carousel from "../components/Carousel";
+import Creators from "../components/Creators";
+import Loading from "../components/Loading";
 
-import { ProjectContext } from '../context';
+import { ProjectContext } from "../context";
 
 export default function Home() {
     const { featured, loading } = useContext(ProjectContext);
@@ -16,25 +16,40 @@ export default function Home() {
     return (
         <>
             <Hero>
-                <Banner title="Programming as a lifestyle" subtitle="An ode to programming / portfolio">
-                    <p className="smaller">by</p>
-                    <p className="bold">Daniel Saavedra</p>
+                <Banner
+                    title='Programming as a lifestyle'
+                    subtitle='An ode to programming | portfolio'
+                >
+                    <p className='smaller'>by</p>
+                    <p className='bold'>Daniel Saavedra</p>
                 </Banner>
             </Hero>
-            <div className="sections-wrapper">
+            <div className='sections-wrapper'>
                 <MainSection title="What's the point of programming?">
-                    <p>In essence, programming is telling a computer what you want it to do in a language it can understand. As such, it doesn't have a set purpose in the same way most computers themselves don't have a set purpose. Coding can be used for data analysis, mathematical modeling, robotics, software and web development, creating beautiful art, simply making your computer say "hello!", and a myriad of other things constrained only by the coder's imagination. That is the beauty of programming and why this page is dedicated to the act itself and the different ways people employ it in their lives.</p>
+                    <p>
+                        In essence, programming is telling a computer what you
+                        want it to do in a language it can understand. As such,
+                        it doesn't have a set purpose in the same way most
+                        computers themselves don't have a set purpose. Coding
+                        can be used for data analysis, mathematical modeling,
+                        robotics, software and web development, creating
+                        beautiful art, simply making your computer say "hello!",
+                        and a myriad of other things constrained only by the
+                        coder's imagination. That is the beauty of programming
+                        and why this page is dedicated to the act itself and the
+                        different ways people employ it in their lives.
+                    </p>
                 </MainSection>
-                <MainSection title="How do people use it? - Practical applications">
+                <MainSection title='How do people use it? - Practical applications'>
                     <Testimonials />
                 </MainSection>
-                <MainSection title="How do people use it? - Creativity">
+                <MainSection title='How do people use it? - Creativity'>
                     <Creators />
                 </MainSection>
-                <MainSection title="How do people use it? - My featured projects">
+                <MainSection title='How do people use it? - My featured projects'>
                     {loading ? <Loading /> : <Carousel data={featured} />}
                 </MainSection>
             </div>
         </>
-    )
+    );
 }
