68fae5334d1c50f65b0dc1bf86343a2020e859c1
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-01-07 08:50:43 -0500

    Added custom analytics events

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,38 @@
 import React from "react";
+import ReactGA from "react-ga";
 
-import {
-    FaCodepen,
-    FaGithub,
-    FaInstagram,
-    FaEnvelope,
-    FaLinkedin,
-} from "react-icons/fa";
+import { FaCodepen, FaGithub, FaInstagram, FaEnvelope, FaLinkedin } from "react-icons/fa";
 import { RiHeart3Line } from "react-icons/ri";
 
 export default function Footer() {
+    function track() {
+        ReactGA.event({
+            category: "Social",
+            action: "User clicked on social link in footer"
+        });
+    }
+
     return (
         <footer>
             <div className='copyright'>&copy; 2020. All Rights Reserved.</div>
             <div className='designedBy'>
                 Designed with <RiHeart3Line className='heart' /> by{" "}
-                <a
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    href='https://github.com/dsaavedr'
-                >
+                <a target='_blank' rel='noopener noreferrer' href='https://github.com/dsaavedr'>
                     Daniel Saavedra
                 </a>
                 .
             </div>
             <div className='icons'>
                 {/* prettier-ignore */}
-                <a target="_blank" rel="noopener noreferrer" href="https://codepen.io/dsaavedr"><FaCodepen /></a>
+                <a onClick={track} target="_blank" rel="noopener noreferrer" href="https://codepen.io/dsaavedr"><FaCodepen /></a>
                 {/* prettier-ignore */}
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/dsaavedr"><FaGithub /></a>
+                <a onClick={track} target="_blank" rel="noopener noreferrer" href="https://github.com/dsaavedr"><FaGithub /></a>
                 {/* prettier-ignore */}
-                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/danielsm14/"><FaInstagram /></a>
+                <a onClick={track} target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/danielsm14/"><FaInstagram /></a>
                 {/* prettier-ignore */}
-                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/daniel-saavedra-988717121/"><FaLinkedin /></a>
+                <a onClick={track} target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/daniel-saavedra-988717121/"><FaLinkedin /></a>
                 {/* prettier-ignore */}
-                <a target="_blank" rel="noopener noreferrer" href="mailto:danielsaavedram@hotmail.com"><FaEnvelope /></a>
+                <a onClick={track} target="_blank" rel="noopener noreferrer" href="mailto:danielsaavedram@hotmail.com"><FaEnvelope /></a>
             </div>
         </footer>
     );
--- src/components/PortCard.js
+++ src/components/PortCard.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from "react";
+import ReactGA from "react-ga";
 
-import { FaLink } from 'react-icons/fa';
+import { FaLink } from "react-icons/fa";
 
 export default function PortCard({ data }) {
+    function track() {
+        ReactGA.event({
+            category: "Project",
+            action: "User clicked on project link"
+        });
+    }
+
     return (
-        <div className="portCard" >
+        <div className='portCard'>
             <img src={data.img} alt={data.title} />
-            <div className="content">
+            <div className='content'>
                 <h4>{data.title}</h4>
                 <p>{data.description}</p>
-                {data.url ? <a href={data.url} target="_blank" rel="noopener noreferrer"><FaLink /></a> : <FaLink className="inactive" />}
+                {data.url ? (
+                    <a onClick={track} href={data.url} target='_blank' rel='noopener noreferrer'>
+                        <FaLink />
+                    </a>
+                ) : (
+                    <FaLink className='inactive' />
+                )}
             </div>
         </div>
-    )
+    );
 }
