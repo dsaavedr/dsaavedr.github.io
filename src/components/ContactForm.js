fe83d6c4a2248ded8de91df557ffcdfd47fadc9c
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-01-12 18:50:10 -0500

    Removed PHP handling. Gonna roll Express app to handle submissions

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,43 +5,92 @@ export default class ContactForm extends Component {
         super();
 
         this.state = {
-            from: "",
-            email: "",
             name: "",
+            email: "",
+            subject: "",
             message: ""
         };
+
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onNameChange = this.onNameChange.bind(this);
+        this.onSubjectChange = this.onSubjectChange.bind(this);
+        this.onMessageChange = this.onMessageChange.bind(this);
+    }
+
+    onEmailChange(e) {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    onNameChange(e) {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    onSubjectChange(e) {
+        this.setState({
+            subject: e.target.value
+        });
+    }
+
+    onMessageChange(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
     }
 
     render() {
         return (
             <div id='contact-form'>
-                <form action='./contactform.php' method='post'>
+                <form onSubmit={this.handleSubmit.bind(this)} method='POST'>
                     <div className='body'>
                         <div id='header'>
                             <div className='row'>
                                 <label className='form-label' htmlFor='name'>
                                     Full name
                                 </label>
-                                <input className='form-control' type='text' name='name' />
+                                <input
+                                    className='form-control'
+                                    type='text'
+                                    name='name'
+                                    onChange={this.onNameChange}
+                                />
                             </div>
                             <div className='row'>
                                 <label className='form-label' htmlFor='mail'>
                                     Your e-mail
                                 </label>
-                                <input className='form-control' type='email' name='mail' />
+                                <input
+                                    className='form-control'
+                                    type='email'
+                                    name='mail'
+                                    onChange={this.onEmailChange}
+                                />
                             </div>
                             <div className='row'>
                                 <label htmlFor='subject' className='form-label'>
                                     Subject
                                 </label>
-                                <input className='form-control' type='text' name='subject' />
+                                <input
+                                    className='form-control'
+                                    type='text'
+                                    name='subject'
+                                    onChange={this.onSubjectChange}
+                                />
                             </div>
                         </div>
                         <div id='message'>
                             <label htmlFor='message' className='form-label'>
                                 Message
                             </label>
-                            <textarea name='message'></textarea>
+                            <textarea name='message' onChange={this.onMessageChange}></textarea>
                         </div>
                     </div>
                     <button
