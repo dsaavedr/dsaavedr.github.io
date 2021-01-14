c4122a1f49cb17cd36b94201b660d2396febb695
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-01-13 19:30:25 -0500

    Animated appearance

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useRef, useEffect } from "react";
+
+import { gsap } from "gsap";
 
 export default function Banner({ children, title, subtitle }) {
+    const bannerRef = useRef(null);
+
+    useEffect(() => {
+        gsap.from(bannerRef.current, {
+            autoAlpha: 0,
+            ease: "out",
+            duration: 1,
+            y: 100
+        });
+    }, []);
+
     return (
-        <div className="banner">
+        <div className='banner' ref={bannerRef}>
             <h1>{title}</h1>
             <div></div>
             <p>{subtitle}</p>
             {children}
         </div>
-    )
+    );
 }
