3ba9329c00066b400ca925cfe7b6685448977d6d
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-01-13 19:30:15 -0500

    Minor formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,17 +27,15 @@ export default function Home() {
             <div className='sections-wrapper'>
                 <MainSection title="What's the point of programming?">
                     <p>
-                        In essence, programming is telling a computer what you
-                        want it to do in a language it can understand. As such,
-                        it doesn't have a set purpose in the same way most
-                        computers themselves don't have a set purpose. Coding
-                        can be used for data analysis, mathematical modeling,
-                        robotics, software and web development, creating
-                        beautiful art, simply making your computer say "hello!",
-                        and a myriad of other things constrained only by the
-                        coder's imagination. That is the beauty of programming
-                        and why this page is dedicated to the act itself and the
-                        different ways people employ it in their lives.
+                        In essence, programming is telling a computer what you want it to do in a
+                        language it can understand. As such, it doesn't have a set purpose in the
+                        same way most computers themselves don't have a set purpose. Coding can be
+                        used for data analysis, mathematical modeling, robotics, software and web
+                        development, creating beautiful art, simply making your computer say
+                        "hello!", and a myriad of other things constrained only by the coder's
+                        imagination. That is the beauty of programming and why this page is
+                        dedicated to the act itself and the different ways people employ it in their
+                        lives.
                     </p>
                 </MainSection>
                 <MainSection title='How do people use it? - Practical applications'>
