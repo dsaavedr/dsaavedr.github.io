0437b0486f92290ea55630902a3c0772fbf1fd03
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-01-16 23:46:53 -0500

    Added parametric message

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import loadingGif from '../images/gif/loading-eclipse.gif';
+import loadingGif from "../images/gif/loading-eclipse.gif";
 
-export default function Loading() {
+export default function Loading(props) {
     return (
-        <div className="loading">
-            <h4>Loading...</h4>
-            <img src={loadingGif} alt="Loading..." />
+        <div className='loading'>
+            <h4>{props.msg || "Loading"}...</h4>
+            <img src={loadingGif} alt='Loading...' />
         </div>
-    )
+    );
 }
