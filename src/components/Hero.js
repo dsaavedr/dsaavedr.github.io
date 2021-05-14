43941a32c7cca6679bd8491d2e8caa9559383440
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-05-13 23:20:07 -0500

    Added iframe with shy-particles project as background

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Hero({ children }) {
     return (
-        <header className="hero">
+        <header className='hero'>
+            <iframe
+                title='background-animation'
+                src='https://dsaavedr.github.io/shy-particles/'
+                styles='height: 100vh; width: 100vw'
+                frameborder='0'
+            ></iframe>
             {children}
         </header>
-    )
+    );
 }
